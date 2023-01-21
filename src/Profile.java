adc01d91da67e209c6edb9c80144446c66e58687
Olivia Liang <olilia26.ubc@gmail.com>
2023-01-21 14:36:07 -0800

    JavaScript setup

--- src/Profile.java
+++ src/Profile.java
@@ -1,12 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-//
 
 public class Profile {
-
-    private int age;
-    private Major major;
-    private List<String> intrests = new ArrayList<>();
 
     public Profile(int age, Major major) {
         this.age = age;
---
+++ src/profile.js
@@ -0,0 +1,14 @@
+<script>
+
+class Profile {
+  constructor(name, age, major, interests, personality, availability) {
+    this.name = name;
+    this.year = year;
+    var interests = new Array();
+    this.personality = personality;
+    this.availability = availability;
+
+  }
+}
+
+</script>
