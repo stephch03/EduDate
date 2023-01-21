9abcb16cf2f2e7ca9f72d2e13a308d6c4e9a0794
Olivia Liang <olilia26.ubc@gmail.com>
2023-01-21 13:54:18 -0800

    first commit

---
+++ src/Gender.java
@@ -0,0 +1,3 @@
+public enum Gender {
+    SHE, HE, THEY,
+}
---
+++ src/Profile.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profile {
+
+    private int age;
+    private Major major;
+    private List<String> intrests = new ArrayList<>();
+
+    public Profile(int age, Major major) {
+        this.age = age;
+        this.major = major;
+    }
+
+}
